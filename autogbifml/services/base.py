d9565e31c4705a11ea7911e07cf9545cce47ffa7
fahminlb33 <fahmi@kodesiana.com>
2024-12-11 10:27:59 +0700

    chore: debug mode logging

--- autogbifml/services/base.py
+++ autogbifml/services/base.py
@@ -6,11 +6,11 @@ class BaseCommand:
     def __init__(self) -> None:
         # setup logging
         self.logger = logging.getLogger(type(self).__name__)
-        self.logger.propagate = False
+        self.logger.propagate = True
 
         # create console handler and set level to info
         stdout = logging.StreamHandler(stream=sys.stdout)
-        stdout.setLevel(logging.INFO)
+        stdout.setLevel(logging.DEBUG)
 
         # create formatter
         formatter = logging.Formatter(
@@ -21,4 +21,4 @@ class BaseCommand:
         self.logger.addHandler(stdout)
 
         # set level to info
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(logging.DEBUG)
